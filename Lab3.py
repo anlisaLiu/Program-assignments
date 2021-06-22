7431b9059000f4a18f303da968b115a4ec1827fc
anlisaLiu <85902102+anlisaLiu@users.noreply.github.com>
2021-06-22 13:40:48 -0500

    Delete Lab3.py

--- Lab3.py
+++
@@ -1,6 +0,0 @@
-myDict={"green":"verde","pink":"rosa", "red":"rojo"}
-myList=[1,2,6,7]
-muTuple=(0,3,9)
-mySet={"cool", "blue", 10, 8 ,555, "orange", "awesome"}
-
-print (myDict[2], myList[0], myTuple[-1],mySet[-3])
