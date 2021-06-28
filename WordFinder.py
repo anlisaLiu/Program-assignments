c30879f2a6cbf41722923de3a40b416e6f54cc17
anlisaLiu <85902102+anlisaLiu@users.noreply.github.com>
2021-06-28 13:30:43 -0500

    Add files via upload

---
+++ WordFinder.py
@@ -0,0 +1,12 @@
+wordList=("tuple","dictionary","integer","boolean","float","modulus","exponential","iteration","loop","while")
+
+def longWord(wordList):
+    i=0
+    longWord=""
+    while(i<wordList.length):
+        if(len(wordList[i]>len(longWord)):
+            longWord=wordList[i]
+        i+=1
+    return longWord
+
+print("the longest word happens to be"+"/n"+"  Drum rolls.... ..."+"/n"+ longWord(wordList))
